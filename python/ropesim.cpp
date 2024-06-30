f367c253b05ad13967cf0c98ad1f2157bffeb112
shawn-phy <philipoyuga14@gmail.com>
2024-06-30 20:45:48 +0300

    feat: ropesim bindings layer

--- python/ropesim.cpp
+++ python/ropesim.cpp
@@ -1,3 +1,13 @@
 #include <pybind11/pybind11.h>
+#include "ropesim_bits/rope.hpp"
 
 namespace py = pybind11;
+
+void init_rope(py::module &);
+
+PYBIND11_MODULE(ropesim, m) {
+    // Optional docstring
+    m.doc() = "Rope simulation library";
+    
+    init_rope(m);
+}
