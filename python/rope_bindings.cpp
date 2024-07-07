829e0c9c74fd2c561774e3d18c6cde500a7c287d
shawn-phy <philipoyuga14@gmail.com>
2024-07-08 02:20:41 +0300

    feat: updated particle struct

--- python/rope_bindings.cpp
+++ python/rope_bindings.cpp
@@ -6,15 +6,27 @@ namespace py = pybind11;
 #include "../cpp/include/ropesim_bits/rope.hpp"
 
 void init_rope(py::module &m) {
+    py::class_ <simlogic::particle>(m, "particle")
+        .def(py::init())
+        .def_readwrite("x", &simlogic::particle::x)
+        .def_readwrite("y", &simlogic::particle::y)
+        .def_readwrite("xp", &simlogic::particle::xp)
+        .def_readwrite("yp", &simlogic::particle::yp)
+        .def_readwrite("fixed", &simlogic::particle::fixed);
+
+
+
+
     py::class_<simlogic::rope>(m, "rope")
         .def(py::init<float, float, float, float, unsigned int>(), py::arg("x1"), py::arg("x2"), py::arg("y1"), py::arg("y2"), py::arg("nparticles"))
         .def("step", &simlogic::rope::step)
         .def("verletintegration", &simlogic::rope::verletintegration)
-        .def("enforceconstraints", &simlogic::rope::enforceconstraints);
+        .def("enforceconstraints", &simlogic::rope::enforceconstraints)
+        .def_readwrite("_particles", &simlogic::rope::_particles)
+        .def_readwrite("_desireddistance", &simlogic::rope::_desireddistance);
 
-    m.def("gravity", &simlogic::gravity);
-    m.def("dt", &simlogic::dt);
-    m.def("max_iterations", &simlogic::max_iterations);
-    m.def("_particles", &simlogic::rope::_particles);
-    m.def("_desireddistance", &simlogic::rope::_desireddistance);
+
+    m.attr("gravity") = simlogic::gravity;
+    m.attr("max_iterations") = simlogic::max_iterations;
+    m.attr("dt") = simlogic::dt;
 }
