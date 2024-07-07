60909a2cee166e72c2a6c7a93329e9aaa235d237
shawn-phy <philipoyuga14@gmail.com>
2024-07-07 23:01:51 +0300

    feat: added those wretched classes,

--- python/rope_bindings.cpp
+++ python/rope_bindings.cpp
@@ -9,4 +9,12 @@ void init_rope(py::module &m) {
     py::class_<simlogic::rope>(m, "rope")
         .def(py::init<float, float, float, float, unsigned int>(), py::arg("x1"), py::arg("x2"), py::arg("y1"), py::arg("y2"), py::arg("nparticles"))
         .def("step", &simlogic::rope::step);
+        .def("verletintegration", &simlogic::rope::verletintegration);
+        .def("enforceconstraints", &simlogic::rope::enforceconstraints);
+
+    // m.def("gravity", &simlogic::gravity);
+    // m.def("dt", &simlogic::dt);
+    // m.def("max_iterations", &simlogic::max_iterations);
+    // m.def("_particles", &simlogic::_particles);
+    // m.def("_desireddistance", &simlogic::_desireddistance);
 }
